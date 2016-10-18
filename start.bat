703b5e6c4367d684ac732305251176e74d59d1de
onway-ws <v.silin@bk.ru>
2016-10-18 15:14:15 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,8 +4,8 @@ if not exist .drv\sync.exe .scr\download
 if not exist .drv\.sync md .drv\.sync
 if not exist inst  md inst
 .scr\rep .drv\sync.conf __F1__ "%CD%\inst"
-.scr\rep .drv\sync.conf __F1__ "%CD%\inst" 
-
+.scr\rep .drv\sync.conf __SECRET_KEY_F1__ "A4PKCWUJWF6RUUA4PS3OVSA5JBINHE3K5" 
+REM .scr\rep .drv\sync.conf __SECRET_KEY_F1__ "BSTZWUMOB63ZASZLUB6QCJBNIMJUY7BJ2"
 .scr\rep .drv\sync.conf \ / 
 cd .drv
 start sync.exe /config sync.conf
