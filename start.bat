234c23002b841cebc491b71b552ea9ad82fe49b5
onway-ws <v.silin@bk.ru>
2016-10-18 14:34:29 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -2,10 +2,10 @@ if not exist .drv md .drv
 copy /Y .scr\sync.tpl .drv\sync.conf
 if not exist .drv\sync.exe .scr\download
 if not exist .drv\.sync md .drv\.sync
-if not exist devl  md devl
 if not exist inst  md inst
+.scr\rep .drv\sync.conf __F1__ "%CD%\inst"
 .scr\rep .drv\sync.conf __F1__ "%CD%\inst" 
-.scr\rep .drv\sync.conf __F2__ "%CD%\devl" 
+
 .scr\rep .drv\sync.conf \ / 
 cd .drv
 start sync.exe /config sync.conf
